ca36c1a4496197949c6b10b0e99eb926bcdf2317
Naresh Kumar <nareshkmr219@gmail.com>
2023-05-12 23:07:45 +0530

    removed unworking code

--- organisation.model.ts
+++ organisation.model.ts
@@ -1,42 +1,40 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 // Define the schema for the organisation
-const OrganisationSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+const OrganisationSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        website: {
+            type: String,
+            optional: true,
+        },
+        description: {
+            type: String,
+            optional: true,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
+    {
+        virtuals: {
+            orgId: {
+                get() {
+                    return this._id;
+                },
+            },
+        },
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    website: {
-        type: String,
-        optional: true,
-    },
-    description: {
-        type: String,
-        optional: true,
-    },
-});
-
-// Hash the password before saving
-OrganisationSchema.pre('save', function (next) {
-    if (this.isModified('password')) {
-        // Hash the password
-        const hash = bcrypt.hashSync(this.password, 10);
-
-        // Set the password to the hash
-        this.password = hash;
-    }
-
-    next();
-});
+);
 
 // Create the model
 const Organisation = mongoose.model('Organisation', OrganisationSchema);
