6222d9c8995dd382feeebc2290e8316ad2f9dc28
Naresh Kumar <nareshkmr219@gmail.com>
2023-05-12 18:43:37 +0530

    org models

---
+++ organisation.model.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+// Define the schema
+const OrganisationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    website: {
+        type: String,
+        optional: true,
+    },
+    description: {
+        type: String,
+        optional: true,
+    },
+});
+
+// Hash the password before saving
+OrganisationSchema.pre('save', function (next) {
+    if (this.isModified('password')) {
+        // Hash the password
+        const hash = bcrypt.hashSync(this.password, 10);
+
+        // Set the password to the hash
+        this.password = hash;
+    }
+
+    next();
+});
+
+// Create the model
+const Organisation = mongoose.model('Organisation', OrganisationSchema);
+export default Organisation;
